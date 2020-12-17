d1ba92851110b7edabe52524b54a96641d5bed86
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-17 00:26:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,15 +61,15 @@ def monopoly_with_fourth_tier
 		}
 	end
 
-	def monopoly_with_third_tier
-		monopoly = {
-			:railroads =>
+def monopoly_with_third_tier
+	monopoly = {
+		:railroads =>
+		{
+			:pieces => 4,
+			:names =>
 			{
-				:pieces => 4,
-				:names =>
+				:reading_railroad =>
 				{
-					:reading_railroad =>
-					{
 						"mortgage_value" => "$100"
 					},
 					:pennsylvania_railroad => {},
