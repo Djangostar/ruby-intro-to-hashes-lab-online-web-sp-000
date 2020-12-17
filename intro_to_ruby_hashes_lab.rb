b1a4304c5ae587052a44e5df3af4cef3dc07abf4
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-17 00:24:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,14 +31,14 @@ def monopoly_with_third_tier
 		:railroads =>
 		{
 			:pieces => 4,
-			:names => 
+			:names =>
 			{
 				:reading_railroad => {},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
 			},
-			:rent_in_dollars => 
+			:rent_in_dollars =>
 			{
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
@@ -66,9 +66,9 @@ def monopoly_with_fourth_tier
 			:railroads =>
 			{
 				:pieces => 4,
-				:names => 
+				:names =>
 				{
-					:reading_railroad => 
+					:reading_railroad =>
 					{
 						"mortgage_value" => "$100"
 					},
@@ -76,7 +76,7 @@ def monopoly_with_fourth_tier
 					:b_and_o_railroad => {},
 					:shortline => {}
 				},
-				:rent_in_dollars => 
+				:rent_in_dollars =>
 				{
 					:one_piece_owned => 25,
 					:two_pieces_owned => 50,
